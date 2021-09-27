93638a550942982c348028c90f50e08f68a18c3a
Paul Gallegos Aguilar <fher.dream6@gmail.com>
2021-09-26 23:28:18 -0500

    change sqlite to csv file

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -2,14 +2,15 @@ module Api
     module V1
         class PokemonsController < ApplicationController
             require 'will_paginate/array'
+            require "csv"
             def index
+                #get data from csv
+                pokemonsTmp = handlerCSV();
 
-                pokemons = []
-                Pokemon.all.each do |pokemon|
-                    pokemons.push(pokemon)
-                end
-
-                pokemons = pokemons.paginate(page: params[:page], per_page: 10);
+                #Pokemon.all.each do |pokemon|
+                #    pokemons.push(pokemon)
+                #end
+                pokemons = pokemonsTmp.paginate(page: params[:page], per_page: 10);
 
                 render json: {
                     status: 'Success',
@@ -20,46 +21,86 @@ module Api
             end
 
             def show
-                pokemon = Pokemon.find(params[:id]);
+                #pokemon = Pokemon.find(params[:id]);
+                pokemonsTmp = handlerCSV();
+   
+                pokemons = pokemonsTmp.find_all { |data| data.id == params[:id].to_i }
                 render json: {
                     status: 'Success',
                     message: 'Pokemon by id',
-                    data: pokemon
+                    data: pokemons
                 }, status: :ok
+
             end
 
             def create
                 pokemon = Pokemon.new(pokemon_params);
-                if pokemon.save
-                    render json: {
-                        status: 'Success',
-                        message: 'Pokemon Created',
-                        data: pokemon
-                    }, status: :ok
-                else
-                    render json: {
-                        status: 'Success',
-                        message: 'Pokemon no Created',
-                        data: pokemon
-                    }, status: :unprocessable_entity
-                end
+                
+
+            
+                #pokemon = Pokemon.new(pokemon_params);
+                #if pokemon.save
+                #    render json: {
+                #        status: 'Success',
+                #        message: 'Pokemon Created',
+                #        data: pokemon
+                #    }, status: :ok
+                #else
+                #    render json: {
+                #        status: 'Success',
+                #        message: 'Pokemon no Created',
+                #        data: pokemon
+                #    }, status: :unprocessable_entity
+                #end 
+    
             end
 
             def destroy
-                pokemon = Pokemon.find(params[:id]);
-                if pokemon.destroy
+                
+                csv_text = File.read(Rails.root.join("lib", "csvs", "pokemon.csv"))
+                csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+      
+                dataSelected = csv.select{ |row| row['#'] == params[:id]}
+                pokemons = []
+                dataSelected.each do |row| 
+                    pokemons.push(getPokemon(row))
+                end  
+  
+                if pokemons.size > 0
+                    csv.delete_if do |row|
+                        row['#'] == params[:id]
+                    end
+                
+                    File.open(Rails.root.join("lib", "csvs", "pokemon.csv"), 'w') do |f|
+                        f.write(csv)
+                    end
                     render json: {
                         status: 'Success',
                         message: 'Pokemon destroyed',
-                        data: pokemon
+                        data: pokemons
                     }, status: :ok
                 else
                     render json: {
                         status: 'Success',
-                        message: 'Pokemon no destroyed',
-                        data: pokemon
+                        message: 'Pokemon no found',
+                        data: []
                     }, status: :unprocessable_entity
                 end
+
+                #pokemon = Pokemon.find(params[:id]);
+                #if pokemon.destroy
+                #    render json: {
+                #        status: 'Success',
+                #        message: 'Pokemon destroyed',
+                #        data: pokemon
+                 #   }, status: :ok
+                #else
+                #    render json: {
+                #        status: 'Success',
+                #        message: 'Pokemon no destroyed',
+                #        data: pokemon
+                #    }, status: :unprocessable_entity
+                #end
             end
 
             def update
@@ -82,11 +123,62 @@ module Api
             private
 
             def pokemon_params
-                params.permit(:name, :type_one, :type_two,
+                params.permit(:id, :name, :type_one, :type_two,
                     :total, :hp, :attack, :defense, :sp_atk,
                     :sp_def, :speed, :generation, :legendary)
             end
+
+            def handlerCSV()
+                pokemons = []
+                csv_text = File.read(Rails.root.join("lib", "csvs", "pokemon.csv"))
+                csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+                csv.each do |row|
+                    pokemons.push(getPokemon(row))
+                end       
+                return pokemons
+            end
             
+            def getPokemon(row)
+                t = Pokemon.new
+                t.id = row['#']
+                t.name = row['Name']
+                t.type_one = row['Type 1']
+                t.type_two = row['Type 2']
+                t.total = row['Total']
+                t.hp = row['HP']
+                t.attack = row['Attack']
+                t.defense = row['Defense']
+                t.sp_atk = row['Sp. Atk']
+                t.sp_def = row['Sp. Def']
+                t.speed = row['Speed']
+                t.generation = row['Generation']
+                t.legendary = row['Legendary']
+                return t
+            end
+
         end
     end
 end
+
+=begin
+pokemons= []
+                pokemons.push(pokemon)
+                data_csv_data = []
+                pokemons.each do |data|
+                  data_csv_data << {
+                        '#': data[:id],
+                        'Name': data[:name],
+                        'Type 1': data[:type_one],
+                        'Type 2': data[:type_two],
+                        'Total': data[:total],
+                        'HP': data[:hp],
+                        'Attack': data[:attack],
+                        'Defense': data[:defense],
+                        'Sp. Atk': data[:sp_atk],
+                        'Sp. Def': data[:sp_def],
+                        'Speed': data[:speed],
+                        'Generation': data[:generation],
+                        'Legendary': data[:legendary]
+                    }
+                end 
+=end
